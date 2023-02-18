e9bdc529f21f43d7e38dff7de1ec9bcda831cd9b
Jesús Quintana <jesquiliche@hotmail.comm>
2023-02-18 01:17:26 +0100

    api v.2.6

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'API COMERCIO',
+  title: 'API LARAVEL DOC',
   tagline: 'Desarrollo de una API REST en laravel 9',
   favicon: 'assets/images/favicon.ico',
 
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,30 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Facil de usar',
+    title: 'Facíl de usar',
     Svg: require('@site/static/assets/images/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Guía paso a paso con todo el código y comentada paso a paso.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Implantación de JWT',
     Svg: require('@site/static/assets/images/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Guía paso a paso de como implementar Json Web Token en nuestra API.
+        Adaptado a la última versión de Larbel.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Con conceptos',
     Svg: require('@site/static/assets/images/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Con definición de conceptos mas usuales en Laravel 9 (con ejemplos).
       </>
     ),
   },
